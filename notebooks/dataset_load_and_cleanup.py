1c1c6a61df0e1d61f867c67aa287b6238642190e
Sai Krishna <jsaikrishna379@gmail.com>
2023-06-23 16:34:12 -0500

    Committing the cleanup, overlays and plots script

---
+++ notebooks/dataset_load_and_cleanup.py
@@ -0,0 +1,79 @@
+#-*- coding: utf-8 -*-
+## Load Dependencies
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import folium
+import shapely
+import os
+pd.set_option('display.max_columns', None)
+
+##Set Working Directory
+wd = '../data/'
+
+##Load Justice40 Data and clean it
+justice40 = gpd.read_file(wd +'justice40 shapefile/usa/usa.shp')
+cols_to_keep = ['GEOID10','SF', 'CF','TPF','UF_PFS','SN_C','SN_T','HRS_ET','LHE','FPL200S','UI_EXP','THRHLD','geometry']
+justice40 = justice40[cols_to_keep]
+justice40.columns = ['GeoID','State','County','Tot_Pop','Unemp_Per','Disadv?','Disad_Tri?','Underinves','Low_HS_Edu','Low_Income','UI_EXP','THRHLD','geometry']
+justice_final = justice40[(justice40['Disadv?'] == 1) & (justice40['geometry'].is_valid == True)]
+justice_final.rename(columns = {'GeoID':'TractID'}, inplace = True)
+justice_final['Type'] = 'Justice40'
+os.mkdir(wd + 'justice40_final')
+justice_final_path = wd+'justice40_final/justice40_final.shp'
+justice_final = justice_final.to_crs(epsg=3857)
+justice_final.to_file(justice_final_path, driver='ESRI Shapefile')
+
+##Load the Utility Datasets (Rural CoOps and Municipal Utilities) and clean them
+utility_territories = gpd.read_file(wd + 'utility shape file/Electric_Retail_Service_Territories.shp').to_crs(epsg=3857)
+util_cols_to_remove = ['OBJECTID','SOURCE','SOURCEDATE','VAL_METHOD','VAL_DATE']
+utility_territories.drop(util_cols_to_remove, axis = 1, inplace = True)
+utility_territories.rename(columns = {'STATE':'ST_Code'}, inplace = True)
+###Load an external dataset with the state names and abbreviations
+states = pd.read_csv(wd+'states.csv')
+states = states[['Abbreviation','State']].rename(columns = {'Abbreviation':'ST_Code'})
+util_merged = utility_territories.merge(gpd.GeoDataFrame(states), how='left', left_on='ST_Code', right_on='ST_Code')
+os.mkdir(wd + 'util_clean')
+util_clean_path = wd+'util_clean/util_clean.shp'
+util_merged.to_file(util_clean_path, driver='ESRI Shapefile')
+###Separate the utilities into rural cooperatives and municipal utilities
+rural_coops = util_merged.query('(TYPE == "COOPERATIVE")').to_crs(epsg=3857)
+rural_coops['Type'] = 'Rural Co-Op'
+rural_coops['area'] = rural_coops['geometry'].area  
+os.mkdir(wd + 'rural_coops')
+rural_coops_path = wd+'rural_coops/rural_coops.shp'
+rural_coops.to_file(rural_coops_path, driver='ESRI Shapefile')
+
+municipal_utils = util_merged.query('(TYPE == "MUNICIPAL") | (TYPE == "POLICTICAL SUBDIVISION")').to_crs(epsg=3857)
+municipal_utils['Type'] = 'Municipal/Public Utility'
+municipal_utils['area'] = municipal_utils['geometry'].area
+os.mkdir(wd + 'municipal_utils')
+municipal_utils_path = wd+'municipal_utils/municipal_utils.shp'
+municipal_utils.to_file(municipal_utils_path, driver='ESRI Shapefile')
+
+##Load the energy communities datasets and clean it
+##Coal Closure dataset
+coal_shp_path = wd + 'ira_data_coalclosure_energycomm_20231\IRA_Coal_Closure_Energy_Comm_SHP\Coal_Closure_Energy_Communities_shp\Coal_Closure_Energy_Communities.shp'
+coal_df = gpd.read_file(coal_shp_path, driver = 'ESRI Shapefile').to_crs(epsg=3857)
+drop_cols_coal = ['objectid', 'affgeoid_t', 'fipstate_2', 'fipcounty_', 'geoid_coun','fiptract_2','censustrac','date_last_','dataset_ve']
+coal_df.drop(columns=drop_cols_coal, inplace=True)
+coal_df.rename(columns={'geoid_trac':'TractID', 'state_name':'State','county_nam':'County','mine_closu':'mine_clos',
+                          'generator_':'gen_clos','adjacent_t':'adj_clos'}, inplace=True)
+coal_df['Type'] = 'Energy-Coal'
+os.mkdir(wd + 'energy_coal')
+coal_path = wd + 'energy_coal/coal_df.shp'
+coal_df.to_file(coal_path, driver='ESRI Shapefile')
+##Fossil Fuel Employment dataset
+ffe_path = wd + '/ira_data_msanmsa_ffe_20231/MSA_NMSA_FFE_SHP/MSA_NMSA_FFE_SHP.shp'
+ffe_df = gpd.read_file(ffe_path, driver = 'ESRI Shapefile').to_crs(epsg=3857)
+drop_cols_ffe = ['ObjectID','fipstate_2','fipscty_20','geoid_cty_','MSA_area_I','Date_Last_','Dataset_ve']
+ffe_df.drop(columns=drop_cols_ffe, inplace=True)
+ffe_df.rename(columns = {'AFFGEOID_C':'TractIDcty','state_name':'State','county_nam':'County','FFE_qual_s':'is_FFE','MSA_NMSA_L':'M_NMSA_loc',
+                          'Shape_Leng':'Leng_Cty','Shape_Area':'Area_Cty'}, inplace=True)
+ffe_df['Type'] = 'Energy-FFE'
+os.mkdir(wd + 'energy_ffe')
+ffe_df_path = wd + 'energy_ffe/ffe_df.shp'
+ffe_df.to_file(ffe_df_path, driver='ESRI Shapefile')
---
+++ notebooks/overlays_and_maps.py
@@ -0,0 +1,126 @@
+#-*- coding: utf-8 -*-
+## Load Dependencies
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import seaborn as sns
+import folium
+pd.set_option('display.max_columns', None)
+
+##Set Working Directory
+wd = '../data/'
+
+##Load Justice40 Data
+justice_final_path = wd+'justice40_final/justice40_final.shp'
+justice_final = gpd.read_file(justice_final_path, driver='ESRI Shapefile').to_crs(epsg=3857)
+
+##Load the Utility Datasets (Rural CoOps and Municipal Utilities)
+rural_coops_path = wd+'rural_coops/rural_coops.shp'
+municipal_utils_path = wd+'municipal_utils/municipal_utils.shp'
+rural_coops = gpd.read_file(rural_coops_path, driver='ESRI Shapefile').to_crs(epsg=3857)
+municipal_utils= gpd.read_file(municipal_utils_path, driver='ESRI Shapefile').to_crs(epsg=3857)
+
+##Load the Energy Communities datasets
+#Coal Closure
+coal_path = (wd + 'energy_coal/coal_df.shp')
+coal_df = gpd.read_file(coal_path, driver='ESRI Shapefile').to_crs(epsg=3857)
+#Fossil Fuel Unemployment
+ffe_path = (wd + 'energy_ffe/ffe_df.shp')
+ffe_df = gpd.read_file(ffe_path, driver='ESRI Shapefile').to_crs(epsg=3857)
+
+##Making Overlays
+def overlays(rural = 0, municipal = 0, community='j40'):
+    if ((rural == 1) & (community=='j40')):
+        j40_rural_overlay = gpd.overlay(justice_final, rural_coops, how='intersection')
+        j40_rural_overlay['areaInters'] = j40_rural_overlay.geometry.area
+        j40_rural_overlay['perc_cover'] = np.round((j40_rural_overlay['areaInters'] / j40_rural_overlay['area']) * 100 , 4)
+        return j40_rural_overlay
+    elif ((rural == 1) & (community=='coal')):
+        coal_rural_overlay = gpd.overlay(coal_df, rural_coops, how='intersection')
+        coal_rural_overlay['areaInters'] = coal_rural_overlay.geometry.area
+        coal_rural_overlay['perc_cover'] = np.round((coal_rural_overlay['areaInters'] / coal_rural_overlay['area']) * 100 , 4)
+        return coal_rural_overlay
+    elif ((rural == 1) & (community=='ffe')):
+        ffe_rural_overlay = gpd.overlay(ffe_df, rural_coops, how='intersection')
+        ffe_rural_overlay['areaInters'] = ffe_rural_overlay.geometry.area
+        ffe_rural_overlay['perc_cover'] = np.round((ffe_rural_overlay['areaInters'] / ffe_rural_overlay['area']) * 100 , 4)
+        return ffe_rural_overlay
+    elif ((municipal == 1) & (community=='j40')):
+        j40_municipal_overlay = gpd.overlay(justice_final, municipal_utils, how='intersection')
+        j40_municipal_overlay['areaInters'] = j40_municipal_overlay.geometry.area
+        j40_municipal_overlay['perc_cover'] = np.round((j40_municipal_overlay['areaInters'] / j40_municipal_overlay['area']) * 100 , 4)
+        return j40_municipal_overlay
+    elif ((municipal == 1) & (community=='coal')):
+        coal_municipal_overlay = gpd.overlay(coal_df, municipal_utils, how='intersection')
+        coal_municipal_overlay['areaInters'] = coal_municipal_overlay.geometry.area
+        coal_municipal_overlay['perc_cover'] = np.round((coal_municipal_overlay['areaInters'] / coal_municipal_overlay['area']) * 100 , 4)
+        return coal_municipal_overlay
+    elif ((municipal == 1) & (community=='ffe')):
+        ffe_municipal_overlay = gpd.overlay(ffe_df, municipal_utils, how='intersection')
+        ffe_municipal_overlay['areaInters'] = ffe_municipal_overlay.geometry.area
+        ffe_municipal_overlay['perc_cover'] = np.round((ffe_municipal_overlay['areaInters'] / ffe_municipal_overlay['area']) * 100 , 4)
+        return ffe_municipal_overlay
+
+##Making Maps based off of the overlays
+def overlayed_plot(rural=0, municipal=0, State = "Illinois"):
+    if (rural ==1):
+        fig = Figure(width=800,height=700)
+        m = folium.Map(location=(40,-89),zoom_start=7)
+        ##Base Co-op Layer
+        folium.GeoJson(rural_coops[rural_coops['State']=='Illinois'], name='Co-Op Territories Overlay',  style_function=lambda feature: {'fillColor': 'red','color': 'black','opacity':1,'weight': 0.4},
+               tooltip=folium.features.GeoJsonTooltip(fields= ['NAME', 'Type'], aliases=['Co-Op:', 'Type:'], labels=True, sticky=True),
+               popup=None
+               ).add_to(m)
+        ##Overlay J40 Layer
+        intersection_j40_state = overlays(rural=1, community = 'j40')
+        intersection_j40_state = intersection_j40_state[intersection_j40_state['State_1']==State].to_crs(epsg=3857)
+        folium.GeoJson(intersection_j40_state, name='j40 Tract Overlay',  style_function=lambda feature: {'fillColor': 'blue','color': 'black','opacity':1,'weight': 0.4},
+               tooltip=folium.features.GeoJsonTooltip(fields=['NAME','County', 'TractID', 'Tot_Pop','perc_cover', 'Type_1'], aliases=['Co-Op:', 'County:', 'Tract ID:', 'Population:','Percent Area Covered:','Type:'],labels=True, sticky=True),
+                popup=None  # Disable the popup
+                ).add_to(m)
+        ##Overlay Coal Layer
+        intersection_coal_state = overlays(rural=1, community = 'coal')
+        intersection_coal_state = intersection_coal_state[intersection_coal_state['State_1']=='Illinois'].to_crs(epsg=3857)
+        folium.GeoJson(intersection_coal_state, name='Coal Communities Overlay',  style_function=lambda feature: {'fillColor': 'green','color': 'black','opacity':1,'weight': 0.4},
+               tooltip=folium.features.GeoJsonTooltip(fields=['NAME','County', 'TractID', 'label','perc_cover', 'Type_1'], aliases=['Co-Op:', 'County:', 'TractID:', 'Label:','Percent Area Covered:','Type:'],labels=True, sticky=True),
+                popup=None  # Disable the popup
+                ).add_to(m)
+        ##Overlay FFE Layer
+        intersection_ffe_state = overlays(rural=1, community = 'ffe')
+        intersection_ffe_state = intersection_ffe_state[intersection_ffe_state['State_1']=='Illinois'].to_crs(epsg=3857)
+        folium.GeoJson(intersection_ffe_state, name='FFe MSA/NMSA Overlay',  style_function=lambda feature: {'fillColor': 'yellow','color': 'black','opacity':1,'weight': 0.4},
+               tooltip=folium.features.GeoJsonTooltip(fields=['NAME','County', 'TractIDcty', 'MSA_NMSA','perc_cover', 'Type_1'], aliases=['Co-Op:', 'County:', 'CountyID:', 'MSA/NMSA:','Percent Area Covered:','Type:'],labels=True, sticky=True),
+                popup=None  # Disable the popup
+                ).add_to(m)
+        fig.add_child(m)
+    if (municipal ==1):
+        fig = Figure(width=800,height=700)
+        m = folium.Map(location=(40,-89),zoom_start=7)
+        ##Base Co-op Layer
+        folium.GeoJson(municipal_utils[municipal_utils['State']=='Illinois'], name='Municipal Utility Territories Overlay',  style_function=lambda feature: {'fillColor': 'red','color': 'black','opacity':1,'weight': 0.4},
+               tooltip=folium.features.GeoJsonTooltip(fields= ['NAME','Type'], aliases=['Utility:','Type:'], labels=True, sticky=True),
+               popup=None
+               ).add_to(m)
+        ##Overlay J40 Layer
+        intersection_j40_state = overlays(municipal=1, community = 'j40')
+        intersection_j40_state = intersection_j40_state[intersection_j40_state['State_1']==State].to_crs(epsg=3857)
+        folium.GeoJson(intersection_j40_state, name='j40 Tract Overlay',  style_function=lambda feature: {'fillColor': 'blue','color': 'black','opacity':1,'weight': 0.4},
+               tooltip=folium.features.GeoJsonTooltip(fields=['NAME','County', 'TractID', 'Tot_Pop','perc_cover', 'Type_1'], aliases=['Utility:', 'County:', 'Tract ID:', 'Population:','Percent Area Covered:','Type:'],labels=True, sticky=True),
+                popup=None  # Disable the popup
+                ).add_to(m)
+        ##Overlay Coal Layer
+        intersection_coal_state = overlays(municipal=1, community = 'coal')
+        intersection_coal_state = intersection_coal_state[intersection_coal_state['State_1']=='Illinois'].to_crs(epsg=3857)
+        folium.GeoJson(intersection_coal_state, name='Coal Communities Overlay',  style_function=lambda feature: {'fillColor': 'green','color': 'black','opacity':1,'weight': 0.4},
+               tooltip=folium.features.GeoJsonTooltip(fields=['NAME','County', 'TractID', 'label','perc_cover', 'Type_1'], aliases=['Utility:', 'County:', 'TractID:', 'Label:','Percent Area Covered:','Type:'],labels=True, sticky=True),
+                popup=None  # Disable the popup
+                ).add_to(m)
+        ##Overlay FFE Layer
+        intersection_ffe_state = overlays(municipal=1, community = 'ffe')
+        intersection_ffe_state = intersection_ffe_state[intersection_ffe_state['State_1']=='Illinois'].to_crs(epsg=3857)
+        folium.GeoJson(intersection_ffe_state, name='FFe MSA/NMSA Overlay',  style_function=lambda feature: {'fillColor': 'yellow','color': 'black','opacity':1,'weight': 0.4},
+               tooltip=folium.features.GeoJsonTooltip(fields=['NAME','County', 'TractIDcty', 'MSA_NMSA','perc_cover', 'Type_1'], aliases=['Utility:', 'County:', 'CountyID:', 'MSA/NMSA:','Percent Area Covered:','Type:'],labels=True, sticky=True),
+                popup=None  # Disable the popup
+                ).add_to(m)
+        fig.add_child(m)
+    return fig
