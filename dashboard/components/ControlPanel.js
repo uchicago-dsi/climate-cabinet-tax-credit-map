70238062a3a688033fdeff2fefe45c77424842dc
toddnief <todd.nief@gmail.com>
2023-08-21 17:05:53 -0500

    initial scaffold of next app file structure

---
+++ dashboard/components/ControlPanel.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ControlPanel() {
+  return (
+    <div>
+      <h5>Layers</h5>
+    </div>
+  );
+}
---
+++ dashboard/components/Footer.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <div>
+      <h4>Climate Cabinet</h4>
+    </div>
+  );
+}
