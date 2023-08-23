2329f3fd71c4494173207d42ad9791eea9f0e7c2
toddnief <todd.nief@gmail.com>
2023-08-23 18:37:15 -0500

    add climate cabinet logo

--- dashboard/components/Header.js
+++ dashboard/components/Header.js
@@ -1,9 +1,20 @@
 "use client";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="text-center">
-      <h1 className="text-2xl">Climate Cabinet's Good Site</h1>
+    <div className="flex flex-row text-center">
+      <div className="w-[200px]">
+        <Image
+          src="/images/climate-cabinet-logo.png"
+          alt="Description"
+          width={424} // width of the original image
+          height={276} // height of the original image
+        />
+      </div>
+      <div className="flex-1">
+        <h1 className="text-2xl">Climate Cabinet's Good Site</h1>
+      </div>
     </div>
   );
 }
