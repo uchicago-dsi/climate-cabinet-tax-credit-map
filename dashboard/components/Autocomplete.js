be19b16184b5430fc700da96f25bd9ab0e15421c
toddnief <todd.nief@gmail.com>
2023-08-24 10:52:49 -0500

    draft of autocomplete search bar

---
+++ dashboard/components/Autocomplete.js
@@ -0,0 +1,53 @@
+"use client";
+import { memo, useRef, useState } from "react";
+import classNames from "classnames";
+
+function Autocomplete(props) {
+  const { items, value, onChange } = props;
+  const ref = useRef(null);
+  // TODO: standard question about local vs global state
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div
+      className={classNames({
+        "dropdown w-full": true,
+        "dropdown-open": open,
+      })}
+      ref={ref}
+    >
+      <input
+        type="text"
+        className="input input-bordered w-full"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Type something.."
+        tabIndex={0}
+      />
+      <div className="dropdown-content bg-base-200 top-14 max-h-96 overflow-auto flex-col rounded-md">
+        <ul
+          className="menu menu-compact"
+          style={{ width: ref.current?.clientWidth }}
+        >
+          {items.map((item, index) => {
+            return (
+              <li
+                key={index}
+                tabIndex={index + 1}
+                onClick={() => {
+                  onChange(item);
+                  setOpen(false);
+                }}
+                className="border-b border-b-base-content/10 w-full"
+              >
+                <button>{item}</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default memo(Autocomplete);
--- dashboard/components/SearchBar.js
+++ dashboard/components/SearchBar.js
@@ -1,21 +1,27 @@
 "use client";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { memo, useState } from "react";
+import Autocomplete from "./Autocomplete";
 
 export default function SearchBar() {
+  const [value, setValue] = useState("");
+  const states = ["Illinois", "California"];
+
   return (
     <div className="flex flex-col items-center">
       <div>
         <h1>Map Search Tool</h1>
       </div>
-      <div className="relative m-2">
-        <input
+      <div className="relative m-2 z-50">
+        <Autocomplete value={value} onChange={setValue} items={states} />
+        {/* <input
           type="text"
           placeholder="Search..."
           className="border border-gray-300 rounded pl-8 p-1"
         />
         <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500">
-          <i className="fas fa-search"></i> {/* Font Awesome Search Icon */}
-        </span>
+          <i className="fas fa-search"></i>
+        </span> */}
       </div>
     </div>
   );
