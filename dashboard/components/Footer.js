4e8039dd32080a943a7a0e49467c3f218a7c62dd
toddnief <todd.nief@gmail.com>
2023-08-30 09:21:34 -0500

    add footer placeholder

--- dashboard/components/Footer.js
+++ dashboard/components/Footer.js
@@ -1,9 +1,22 @@
 "use client";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <div className="text-center">
-      <h4>Climate Cabinet's Informative Footer</h4>
+      <div className="mx-auto w-[250px]">
+        <Image
+          src="/images/climate-cabinet-logo-black.png"
+          alt="Description"
+          width={424} // width of the original image
+          height={276} // height of the original image
+        />
+      </div>
+      <p className=" w-2/3 mx-auto py-5">
+        Paid Pol. Adv. paid for with regulated funds by Climate Cabinet PAC,
+        independently of and not authorized or approved by any federal, state,
+        or local candidate, candidate’s agent, or committee.
+      </p>
     </div>
   );
 }
