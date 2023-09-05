e4a9564eab84fe68f0a0dd91e966d9539440faa8
toddnief <todd.nief@gmail.com>
2023-09-05 12:26:19 -0500

    add homepage page and routing

---
+++ dashboard/app/home/page.js
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+export default function Homepage() {
+  return (
+    <main className="w-full">
+      <div className="w-full p-5">
+        <Header />
+      </div>
+    </main>
+  );
+}
