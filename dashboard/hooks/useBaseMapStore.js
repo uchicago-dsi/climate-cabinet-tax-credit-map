8b72f5df1b95a7ca3af8931bfb69d0a74b4f21b9
nofurtherinformation <dhalpern@uchicago.edu>
2023-09-20 09:24:41 -0500

    Simplify basemap store

--- dashboard/hooks/useBaseMapStore.js
+++ dashboard/hooks/useBaseMapStore.js
@@ -1,40 +1,28 @@
 /**
- * A custom hook for managing DeckGL base maps.
+ * A store for basemap
  */
-
-import { bind } from "@/lib/utils";
 import { proxy } from "valtio";
 
-
 const baseMapConfig = {
-    political: {
-        name: "Political Basemap",
-        url: "mapbox://styles/mapbox/streets-v12"
-    },
-    satellite: {
-        name: "Satellite Basemap",
-        url: "mapbox://styles/mapbox/satellite-streets-v12"
-    }
+  political: {
+    name: "Political Basemap",
+    url: "mapbox://styles/mapbox/streets-v12",
+  },
+  satellite: {
+    name: "Satellite Basemap",
+    url: "mapbox://styles/mapbox/satellite-streets-v12",
+  },
 };
 
-
-class BaseMapStore {
-    constructor() {
-        this.options = baseMapConfig;
-        this.selected = baseMapConfig.political;
+const baseMapStore = proxy({
+  options: baseMapConfig,
+  selected: baseMapConfig.political,
+  setMap: (mapType) => {
+    if (!Object.keys(baseMapConfig).includes(mapType)) {
+      return Error(`Received an invalid basemap option, "${mapType}".`);
     }
-
-    setMap(mapType) {
-        if (!Object.keys(baseMapConfig).includes(mapType)) {
-            return Error(`Received an invalid basemap option, "${mapType}".`);
-        };
-        this.selected = baseMapConfig[mapType];
-    }
-}
-
-const useBaseMapStore = () => {
-    return bind(proxy(new BaseMapStore()));
-};
-
-
-export { useBaseMapStore }
+    baseMapStore.selected = baseMapConfig[mapType];
+  },
+});
+const useBaseMapStore = () => baseMapStore;
+export { baseMapStore, useBaseMapStore };
