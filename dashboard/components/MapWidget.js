86113e3a2814c210800205d61c0c49c7d110b61f
toddnief <todd.nief@gmail.com>
2023-11-03 10:38:37 -0500

    make fullscreen apply to report widget

--- dashboard/components/MapWidget.js
+++ dashboard/components/MapWidget.js
@@ -12,7 +12,7 @@ import { MapboxOverlay } from "@deck.gl/mapbox";
 import Tooltip from "@/components/Tooltip";
 import { Map, useControl, FullscreenControl } from "react-map-gl";
 import { useSnapshot } from "valtio";
-import { Suspense } from "react";
+import { useRef, useEffect } from "react";
 import {
   reportStore,
   baseMapStore,
@@ -26,6 +26,8 @@ function MapWidget() {
   const baseMapSnap = useSnapshot(baseMapStore);
   const layerSnap = useSnapshot(layerStore);
   const layerClient = useLayers(reportSnap.report?.geographies, layerStore);
+  const mapRef = useRef(null);
+
   const layers = Object.entries(layerSnap).reduce((layers, [id, state]) => {
     if (state.visible) {
       let lyr = layerClient.getLayer(id);
@@ -33,6 +35,25 @@ function MapWidget() {
     }
     return layers;
   }, []);
+  console.log(viewportStore.current);
+
+  const flyToLocation = (lat, lon, z) => {
+    if (!mapRef.current) return;
+    const map = mapRef.current;
+    map.flyTo({
+      center: [lon, lat],
+      zoom: z,
+    });
+  };
+
+  useEffect(() => {
+    flyToLocation(
+      viewportStore.current.latitude,
+      viewportStore.current.longitude,
+      viewportStore.current.zoom
+    );
+  }, [viewportStore.current]);
+
   return (
     <div
       className="relative overflow-hidden"
@@ -44,8 +65,9 @@ function MapWidget() {
         projection="mercator"
         initialViewState={viewportStore.current}
         controller={true}
+        ref={mapRef}
       >
-        <FullscreenControl position="top-left" />
+        <FullscreenControl position="top-left" containerId="report-widget" />
         <DeckGLOverlay layers={layers} interleaved={true} />
       </Map>
       {/* TODO: add a picking radius here to layers?*/}
