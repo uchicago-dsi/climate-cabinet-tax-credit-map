636cfa59fadc1d0878b541c51568672b9af4e4e8
Launa Greer <greerlauna@gmail.com>
2023-11-06 16:03:35 -0600

    Removes old pipeline code

--- old_pipeline/docker_build_image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-#Build the image
-docker build -t py-docker-climate-cabinet .
-#Run the image with a volume
-docker run -v $(pwd)/data:/app/data -v $(pwd)/results:/app/results py-docker-climate-cabinet
