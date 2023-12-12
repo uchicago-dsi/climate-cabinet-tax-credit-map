1021d62654ce66af8856d54d0f2ddee0e1249989
cpredmond98 <cpredmond98@gmail.com>
2023-12-11 18:31:50 -0600

    Aggressive cursor refreshes

--- pipeline/tax_credit/database_helper.py
+++ pipeline/tax_credit/database_helper.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from math import ceil
 
 from django.db.utils import ProgrammingError
+from django.db import connection
 
 from tax_credit.load_job import LoadJob
 from common.storage import DataReader
@@ -24,15 +25,20 @@ class DatabaseHelper:
             #####
 
             batch_ct = 0
+            cursor_start_time = datetime.now()
             is_calibrated = False
             batch_size = 1
             target_time = timedelta(seconds=15)
             max_time = timedelta(seconds=60)
             while True:
-                logger.info(f"Starting batch {batch_ct}")
                 batch = list(islice(objs, batch_size))
                 if not batch:
                     break
+                logger.info(f"{job.job_name} Starting batch {batch_ct}")
+
+                if datetime.now() - cursor_start_time > timedelta(minutes=1):
+                    logger.info("Cursor has been alive too long, resetting")
+                    connection.close()
                 
                 st_time = datetime.now()
                 if job.unique_fields:
@@ -55,8 +61,8 @@ class DatabaseHelper:
                     
                 # Calibrate proper batch size
                 processing_time = end_time - st_time
-                logger.info(f"Finished batch {batch_ct}")
-                logger.info(f"Batch {batch_ct} processing time : {processing_time}")
+                logger.info(f"{job.job_name} Finished batch {batch_ct}")
+                logger.info(f"{job.job_name} Batch {batch_ct} processing time : {processing_time}")
                 if not is_calibrated and abs(processing_time - target_time) <= timedelta(seconds=3):
                         is_calibrated = True
                         target_time = max_time
@@ -65,34 +71,10 @@ class DatabaseHelper:
 
                 batch_ct += 1
 
-            #####
-
-            # batch_ct = 0
-            # while True:
-            #     logger.info(f"Loading batch {batch_ct + 1} for {job.job_name}")
-            #     batch = list(islice(objs, load_batch_size))
-            #     if not batch:
-            #         break
-            #     if job.unique_fields:
-            #         job.model.objects.bulk_create(
-            #             batch,
-            #             update_conflicts=True,
-            #             unique_fields=job.unique_fields,
-            #             update_fields=job.update_fields,
-            #         )
-            #     else: 
-            #         job.model.objects.bulk_create(
-            #             batch,
-            #             update_fields=job.update_fields,
-            #         )
-            #     batch_ct += 1
-            #     if batch_number_of is not None and batch_ct >= batch_number_of:
-            #         break
         except ProgrammingError as e:
             logger.error(
                 f"Could not process [ {job.job_name} ] record. Check row to model transformation: [ {job.row_to_model} ]"
             )
-            # logger.error(f"ERROR : {e}")
 
     @staticmethod
     def query_and_cache(model, field, filter):
