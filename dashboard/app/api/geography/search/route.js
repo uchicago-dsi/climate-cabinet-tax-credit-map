86138a86fafa8339ed3be583ec5b2aa77c849182
Launa Greer <greerlauna@gmail.com>
2024-02-09 19:55:56 -0600

    Fixes untrimmed whitespace bug in search query

--- dashboard/app/api/geography/search/route.js
+++ dashboard/app/api/geography/search/route.js
@@ -13,20 +13,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 /**
  * Searches for a geography by name and returns the
- * top results as a list of objects with "id" and
- * "name" properties. NOTE: To handle search terms
- * with spaces in Postgres, words must be joined with
- * operators like "boolean and" (&) or "proximity" (<->),
- * which preserves word order.
+ * top results as a list of objects with "id", "name",
+ * and "geography_type" properties. Full-text search
+ * is achieved in PostgreSQL using a combination of 
+ * similarity scores calculated from trigrams ("pg_trgm")
+ * and ranks calculated from normalized vectors of 
+ * lexemes ("tsvector").
  *
  *  @param {NextRequest} - The HTTP request. Contains the
  *      search term and the number of matches to return.
  */
 export async function POST(request) {
-  let { searchTerm, limit } = await request.json();
-  let formattedSearchTerm = searchTerm.replace(/\s+/g, ' & ');
+  let { searchPhrase, limit } = await request.json();
+  let formattedSearchPhrase = searchPhrase.replace(/\s+/g, ' ').trim();
+  let tsquerySearchPhrase = formattedSearchPhrase.replace(/\s+/g, ' & ');
+
   let data = await prisma.$queryRaw`
-    SELECT * FROM
+    SELECT
+      id,
+      name, 
+      geography_type
+    FROM
       (
         SELECT 
           id::varchar(255), 
@@ -34,9 +41,9 @@ export async function POST(request) {
           geography_type,
           ts_rank(
             name_vector, 
-            to_tsquery('english', ${formattedSearchTerm})
-          ) as rank,
-          similarity(${searchTerm}, name) as sml
+            to_tsquery('english', ${tsquerySearchPhrase})
+          ) AS rank,
+          similarity(${formattedSearchPhrase}, name) AS sml
         FROM tax_credit_geography
         WHERE geography_type IN (
           'county',
@@ -47,7 +54,7 @@ export async function POST(request) {
         )
         ORDER BY rank DESC, sml DESC, name ASC
         LIMIT ${limit}
-      ) as results
+      ) AS results
       WHERE rank > 1e-20 OR sml > 0
     `;
   return NextResponse.json({ data });
