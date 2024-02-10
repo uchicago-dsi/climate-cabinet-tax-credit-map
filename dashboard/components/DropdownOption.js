a65878bdac15d04bf95dfa55e98e22538519c312
toddnief <todd.nief@gmail.com>
2024-02-10 14:03:05 -0600

    add geography type to search dropdown

--- dashboard/components/DropdownOption.js
+++ dashboard/components/DropdownOption.js
@@ -2,20 +2,20 @@
  * A dropdown item.
  */
 
-"use client"
+"use client";
 
 function DropdownOption({ index, onClick, item }) {
-    return (
-        <li
-          key={index}
-          tabIndex={index + 1}
-          className="border-b border-b-base-content/10 w-full text-lg"
-        >
-          <button value={item} onClick={onClick}>
-            {item.name.toUpperCase()}
-          </button>
-        </li>
-    );
+  return (
+    <li
+      key={index}
+      tabIndex={index + 1}
+      className="border-b border-b-base-content/10 w-full text-lg"
+    >
+      <button value={item} onClick={onClick}>
+        `${item.name.toUpperCase()} (${item.type.toUpperCase()})`
+      </button>
+    </li>
+  );
 }
 
 export default DropdownOption;
