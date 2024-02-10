5720cf3b20fd7e740f85d71d672fb532f6924804
toddnief <todd.nief@gmail.com>
2024-02-10 14:11:00 -0600

    fix dropdown options

--- dashboard/components/DropdownOption.js
+++ dashboard/components/DropdownOption.js
@@ -12,7 +12,7 @@ function DropdownOption({ index, onClick, item }) {
       className="border-b border-b-base-content/10 w-full text-lg"
     >
       <button value={item} onClick={onClick}>
-        item.name.toUpperCase()
+        {item.name.toUpperCase()}
         {/* `${item.name.toUpperCase()} (${item.type.toUpperCase()})` */}
       </button>
     </li>
