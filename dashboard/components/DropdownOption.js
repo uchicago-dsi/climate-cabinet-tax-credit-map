0e5443310a66123b259ffc9d36a73940aa745881
toddnief <todd.nief@gmail.com>
2024-02-11 11:43:54 -0600

    fix string formatting

--- dashboard/components/DropdownOption.js
+++ dashboard/components/DropdownOption.js
@@ -13,8 +13,7 @@ function DropdownOption({ index, onClick, item }) {
       className="border-b border-b-base-content/10 w-full text-lg"
     >
       <button value={item} onClick={onClick}>
-        {/* {item.name.toUpperCase()} */}
-        `${item.name.toUpperCase()} (${item.geography_type.toUpperCase()})`
+        {`${item.name.toUpperCase()} (${item.geography_type.toUpperCase()})`}
       </button>
     </li>
   );
